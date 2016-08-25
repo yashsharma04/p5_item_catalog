556bfc009feb409db43fc0910e2498696a874f65
Yash Sharma <95yashsharma@gmail.com>
2016-08-25 23:04:56 +0530

    OneLastThing

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , redirect, url_for , request
+from flask import Flask , render_template , redirect, url_for , request , flash , jsonify
 app = Flask(__name__)
 # from sqlalchemy import Column, ForeignKey, Integer, String
 
@@ -35,6 +35,7 @@ def newMenuItem(restaurant_id):
 		newItem = MenuItem(name=request.form['name'],restaurant_id=restaurant_id)
 		session.add(newItem)
 		session.commit()
+		flash("new menu item created")
 		return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
 	else:
 		return render_template('newmenuitem.html',restaurant_id=restaurant_id)		
@@ -60,8 +61,23 @@ def deleteMenuItem(restaurant_id,menu_id):
 		session.commit()
 		return redirect(url_for('restaurantMenu',restaurant_id = restaurant_id))
 	else :
-		return render_template('editmenuitem.html',restaurant_id=restaurant_id)
+		return render_template('deletemenuitem.html',i=itemToDelete)
+
+# api endpoint GET
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id= restaurant_id).all()
+	return jsonify(MenuItems = [i.serialize for i in items ])
+
+# API endpoint 
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')	
+def menuItemJSON(restaurant_id,menu_id):
+	menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+	return jsonify(MenuItem = menuItem.serialize)
 
 if __name__=='__main__':
+	app.secret_key="super_secret_key"
 	app.debug=True 
-	app.run(host='',port=5000)
+	app.run(host='',port=8080)
+
